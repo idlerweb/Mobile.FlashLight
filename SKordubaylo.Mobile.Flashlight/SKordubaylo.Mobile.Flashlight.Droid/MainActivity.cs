5957c194c1a6ed54a5fd91ade1b65714af72f59d
idlerweb <skordubaylo@gmail.com>
2016-09-09 22:35:09 +0300

    begin

--- SKordubaylo.Mobile.Flashlight/SKordubaylo.Mobile.Flashlight.Droid/MainActivity.cs
+++ SKordubaylo.Mobile.Flashlight/SKordubaylo.Mobile.Flashlight.Droid/MainActivity.cs
@@ -2,6 +2,7 @@
 
 using Android.App;
 using Android.Content.PM;
+using Android.Hardware;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
@@ -12,13 +13,71 @@ namespace SKordubaylo.Mobile.Flashlight.Droid
     [Activity(Label = "SKordubaylo.Mobile.Flashlight", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
-        }
-    }
+		Camera camera;
+		Button button;
+		bool cameraFlashLightOn;
+
+	    protected override void OnCreate(Bundle bundle)
+	    {
+		    base.OnCreate(bundle);
+
+		    button = FindViewById<Button>(Resource.Id.myButton);
+
+		    //SetContentView(Resource.Layout.Main);
+
+		    global::Xamarin.Forms.Forms.Init(this, bundle);
+		    LoadApplication(new App());
+	    }
+
+	    protected override void OnResume()
+		{
+			if (!PackageManager.HasSystemFeature(Android.Content.PM.PackageManager.FeatureCamera))
+			{
+				Toast.MakeText(this, "No back-facing camera available", ToastLength.Long);
+				return;
+			}
+
+			if (!PackageManager.HasSystemFeature(Android.Content.PM.PackageManager.FeatureCameraFlash))
+			{
+				Toast.MakeText(this, "No camera flash available", ToastLength.Long);
+				return;
+			}
+
+			camera = Camera.Open();
+			button.Click += button_Click;
+
+			base.OnResume();
+		}
+
+		protected override void OnStop()
+		{
+			button.Click -= button_Click;
+			camera.Release();
+
+			base.OnStop();
+		}
+
+		void button_Click(object sender, EventArgs e)
+		{
+			var parameters = camera.GetParameters();
+
+			if (!cameraFlashLightOn)
+			{
+				parameters.FlashMode = Camera.Parameters.FlashModeTorch;
+
+				camera.SetParameters(parameters);
+				camera.StartPreview();
+				cameraFlashLightOn = true;
+				return;
+			}
+
+			parameters.FlashMode = Camera.Parameters.FlashModeOff;
+
+			camera.SetParameters(parameters);
+			camera.StopPreview();
+			cameraFlashLightOn = false;
+		}
+	}
 }
 
